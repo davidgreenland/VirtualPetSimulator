a5253cb3e4c0927f22f30a67d3d714d34ba5098f
David Greenland <David.Greenland@brighthr.com>
2024-10-15 16:44:52 +0100

    YLW-3184 test(Eat): tests for Eating behaviour when not hungry

---
+++ VirtualPetSimulator.Tests/Models/CatPetTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using VirtualPetSimulator.Models;
+
+namespace VirtualPetSimulator.Tests.Models;
+
+public class CatPetTests
+{
+    [Test]
+    public void Eat_WhenNotHungry_DoesNotEat()
+    {
+        var hasFullEnergy = 10;
+        var isFull = 0;
+        var pet = new CatPet("Simon", hasFullEnergy, isFull);
+
+        pet.Eat();
+
+        Assert.That(pet.Hunger, Is.EqualTo(isFull));
+    }
+}
