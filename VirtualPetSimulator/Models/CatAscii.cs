77669b65077d4181fbe5fad9653dbf7880d148c0
David Greenland <David.Greenland@brighthr.com>
2024-10-17 12:00:57 +0100

    YLW-3184: Adds cat ascii art

---
+++ VirtualPetSimulator/Models/CatAscii.cs
@@ -0,0 +1,18 @@
+﻿namespace VirtualPetSimulator.Models;
+
+public static class CatAscii
+{
+    public static string PlayfulCat => " _._     _,-'\"\"`-._\r\n(,-.`._,'(       |\\`-/|\r\n    `-.-' \\ )-`( , o o)\r\n          `-    \\`_`\"'-";
+
+    public static string SleepingCat => "      |\\      _,,,---,,_\r\nZZZzz /,`.-'`'    -.  ;-;;,_\r\n     |,4-  ) )-,_. ,\\ (  `'-'\r\n    '---''(_/--'  `-'\\_)";
+
+    public static string PeepingCat => "                    /)\r\n           /\\___/\\ ((\r\n           \\`@_@'/  ))\r\n           {_:Y:.}_//\r\n ----------{_}^-'{_}----------";
+
+    public static string LayingCat => "   |\\---/|\r\n   | ,_, |\r\n    \\_`_/-..----.\r\n ___/ `   ' ,\"\"+ \\    \r\n(__...'   __\\    |`.___.';\r\n  (_,...'(_,.`__)/'.....+";
+
+    public static string SittingCat => "\n  ^ ^  \r\n ('Y') )\r\n /   \\/ \r\n(\\|||/)";
+
+    public static string SittingCat2 => " /\\_/\\\r\n( o.o ) \r\n > ^ < )\r\n /   \\/ \r\n(\\|||/)";
+
+    public static string CatInBox => "              ,-.       _,---._ __  / \\\r\n             /  )    .-'       `./ /   \\\r\n            (  (   ,'            `/    /|\r\n             \\  `-\"             \\'\\   / |\r\n              `.              ,  \\ \\ /  |\r\n               /`.          ,'-`----Y   |\r\n              (            ;        |   '\r\n              |  ,-.    ,-'         |  /\r\n              |  | (   |            | /\r\n              )  |  \\  `.___________|/\r\n              `--'   `--'";
+}
